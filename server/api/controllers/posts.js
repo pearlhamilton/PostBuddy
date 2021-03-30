f5bb2679444f53c1065d79df92c89b7558b814a9
Elwin <elwin.carlos09@gmail.com>
2021-03-30 17:14:56 +0100

    finish writing create controller

--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -26,10 +26,10 @@ async function show (req, res) {
 //Make create callback function
 async function create (req, res) {
     try {
-
-
+        const post = await Post.create(req.body);
+        res.status(200).json(post);
     } catch(err) {
-
+        res.status(422).json({err});
     }
 }
 
