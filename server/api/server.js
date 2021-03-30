1b633c9cff69d30a32c0883c106a81d3ee034f55
Elwin <elwin.carlos09@gmail.com>
2021-03-30 16:26:45 +0100

    temp comment out postRoutes

--- server/api/server.js
+++ server/api/server.js
@@ -5,8 +5,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const postRoutes = require('./controllers/posts');
-server.use('/posts', postRoutes);
+// const postRoutes = require('./controllers/posts');
+// server.use('/posts', postRoutes);
 
 //Root route
 server.get('/', (req, res) => res.send('Hi it\'s Elwin and Pearl!  Come and make a post.  :)'));
