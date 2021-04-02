51cbfaf76e1f8a0e9a59584bcf42d7239c8f6b7d
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-02 15:59:04 +0100

    add error to appear on client when post is not found

--- client/js/requests.js
+++ client/js/requests.js
@@ -43,18 +43,46 @@ function updatePage(){
 }
 
 async function getPost(id){
+    try{
     const body = document.querySelector('body')
     body.innerHTML = " "
-    let response = await fetch(`http://localhost:3000/posts/${id}`)
-    .then(r => r.json())    
-    .then(data => renderNewContent(data))
+     let response = await fetch(`http://localhost:3000/posts/${id}`)
+    let data = await response.json()  
+    if (data.err){
+        throwError(data)
+    }
+    renderNewContent(data)
+    }
+    catch (error){
+        console.warn(error);
+
+
+    }
 
 
 }
 
 function renderNewContent(data){
-    const body = document.querySelector('body')
-    body.innerHTML = `${data.title}, ${data.author}, ${data.post}`
+
+    const postDiv = document.createElement('div');
+    const h1 = document.createElement('h1')
+    const h2 = document.createElement('h2')
+    const para = document.createElement('p')
+    postDiv.append(h1)
+    postDiv.append(h2)
+    postDiv.append(para)
+    h1.textContent = data.title
+    h2.textContent = data.author
+    para.textContent = data.post
+    body.append(postDiv)
+    
+
+}
+
+function throwError(data){
+    const h1 = document.createElement('h1')
+    h1.textContent = `${data.err}`
+    body.append(h1)
 
 }
 
